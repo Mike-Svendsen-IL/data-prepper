91443563b1a1ce64e1062a3640bb587acde659a9
Mike Svendsen <mike.svendsen@imaginelearning.com>
2024-05-20 12:37:21 -0500

    Remove plugins that weren't building for me locally.
    I didn't need the below plugins so just removed them
    for the moment for ease of building and updating
    what I cared about.

--- data-prepper-plugins/kafka-plugins/src/main/java/org/opensearch/dataprepper/plugins/kafka/common/KafkaMdc.java
+++
@@ -1,8 +0,0 @@
-/*
- * Copyright OpenSearch Contributors
- * SPDX-License-Identifier: Apache-2.0
- */
-
-package org.opensearch.dataprepper.plugins.kafka.common;public class KafkaMdc {
-    public static final String MDC_KAFKA_PLUGIN_KEY = "kafkaPluginType";
-}
